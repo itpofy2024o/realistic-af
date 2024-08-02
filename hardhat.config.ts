b66cca257ca7227a272f6a8cf10b7ef9877dadbd
itpofy2024o <hungaryisagoodforfornothungry@gmail.com>
2024-08-03 01:48:34 +0800

    this is

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import '@matterlabs/hardhat-zksync-deploy';
+import '@matterlabs/hardhat-zksync-solc';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  defaultNetwork: "zkSyncTestnet",
+  networks: {
+    zkSyncTestnet: {
+      url: "https://testnet.era.zksync.dev",
+      ethNetwork: "goerli", // Can also use a Goerli RPC URL or your Alchemy/Infura project
+      zksync: true
+    },
+  },
 };
 
 export default config;
