5f5caea282947129b040f5fe489d8ca2f96c750f
itpofy2024o <qweasdzxcf1G@proton.me>
2024-08-14 20:51:49 +0800

    fker

--- contracts/JoMe.sol
+++ contracts/JoMe.sol
@@ -2,5 +2,48 @@
 pragma solidity ^0.8.17;
 
 contract JoMe {
-    struct 
+    struct Item {
+        string name;
+        string description;
+        string[] categories;
+        uint256 price;
+        uint256 amount;
+        uint256 viewCount;
+        address seller;
+        address[] buyers;
+    }
+
+    struct Releaser {
+        string name;
+        string description;
+        string email;
+        string phone;
+        uint8 rating;
+        Item[] items;
+        mapping(address =>string[]) comments;
+    }
+
+    struct Receiver {
+        string name;
+        mapping(uint256=>bool) viewedProducts;
+        uint256[] orderedProducts;
+    }
+
+    mapping(address=>Releaser) public releasers;
+    mapping(address=>Receiver) public receivers;
+    mapping(uint256=>Item) public items;
+    uint256 public itemCount;
+
+    event ItemOrder(address indexed receiver, uint256 indexed itemId);
+    event ReleaserUpdated(address indexed releaser);
+    event ItemUpdated(uint256 indexed itemId);
+
+    modifier onlyReleaserOwner() {
+        require(1==1,"Not a releaser owner");
+        _;
+    }
+
+    function registerAReleaser(string memory name,string memory description, string memory email, string memory phone) public {
+        
+    }
 }
